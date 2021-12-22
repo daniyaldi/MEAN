bbe658cbad4e25993b95f3d5504ca64060d30e46
daniyaldi <daniyal.irfan@focusteck.com>
2021-12-22 16:36:20 +0500

    commit

--- NodeJs/controllers/loginController.js
+++ NodeJs/controllers/loginController.js
@@ -4,12 +4,10 @@ var ObjectId= require('mongoose').Types.ObjectId;
 var {Employee} = require('../models/employee');
 
 router.post('/',(req,res)=>{
-    console.log("email",req.body)
+   
     let email=req.body.email;
     let password=req.body.password;
-   
-    console.log("email2 ",email);
-    console.log("password",password)
+
      Employee.findOne({email:email},(err,data)=>{
         console.log(data)
         if(data==null)
